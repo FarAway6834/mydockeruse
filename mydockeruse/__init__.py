490cd2583db0242e5d6b181eb640d8d9a9c1e6d2
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-11 21:40:06 +0900

    __init__.py

--- mydockeruse/__init__.py
+++ mydockeruse/__init__.py
@@ -3,6 +3,6 @@ from docker import from_env as _docker_
 from martialaw.martialaw import martialaw as _clsr
 import builtins as __builtin__
 __builtin__.default_docker_environment = _docker_()
-__F = [[_clsr(_clsr(default_docker_environment.containers.run)(stdin_open=1, tty=1, detach=1)), default_docker_environment.images]]
-__F.append([__F[0][1].pull, _clsr(__F[0][1].remove)(force=1)])
+__F = [(_clsr(_clsr(default_docker_environment.containers.run)(stdin_open=1, tty=1, detach=1)), default_docker_environment.images)]
+(lambda x=__F[0][1]:__F.append([staticmethod(f) for f in (f.pull, _clsr(x.remove)(force=1))]))()
 __builtin__.default_docker_environment_manager = type('',(), dict(install = lambda self, x : __F[1][0], uninstall = lambda self, x : __F[1][1], __call__ = lambda self, x : __F[0][0](x)("/bin/bash")))()
