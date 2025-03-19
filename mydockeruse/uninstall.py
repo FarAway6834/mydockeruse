d8244feee2a6e63aefed895b8203f64876df389f
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-20 08:24:03 +0900

    uninstall.py

--- mydockeruse/uninstall.py
+++ mydockeruse/uninstall.py
@@ -1,1 +1,5 @@
-default_docker_environment_manager.uninstall(input('docker img : '))
+from sys import argv as _a
+
+main = lambda x = input('docker img : ') if len(_a) < 2 else _a[1] : default_docker_environment_manager.uninstall(x)
+
+main()
