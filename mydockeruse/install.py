05781555f88ced1e5c47292e84c0d7374ee97785
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-20 08:23:41 +0900

    install.py

--- mydockeruse/install.py
+++ mydockeruse/install.py
@@ -1,5 +1,5 @@
 from sys import argv as _a
 
-main = lambda x = input('docker img : ') if len(_a) < 2 else _a[1] : default_docker_environment_manager.install(input(x)
+main = lambda x = input('docker img : ') if len(_a) < 2 else _a[1] : default_docker_environment_manager.install(x)
 
 main()
