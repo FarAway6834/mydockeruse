5fdb31d1e1e90b764c2383e3cb34400d88b76d32
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-20 08:30:20 +0900

    __main__.py

--- mydockeruse/__main__.py
+++ mydockeruse/__main__.py
@@ -1,5 +1,9 @@
 from sys import argv as _a
 
-main = lambda x = input('docker img : ') if len(_a) < 2 else _a[1] : default_docker_environment_manager(x)
+@(lambda f : lambda x : f(_a[2] if len(a) > 3 else input('file name : ')) if x == '-r' else x)
+def r(f):
+	with open(f) as fp: return fp.read()
+
+main = lambda x = input('docker img : ') if len(_a) < 2 else r(_a[1]): default_docker_environment_manager(x)
 
 main()
